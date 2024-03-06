4f40143ee51bead509dd057dbd6eaf51e39ffe99
nicolas toro <nicolastororios@gmail.com>
2024-03-06 18:02:05 +0100

    Upload working 100%

--- filmsyl-frontend/app.py
+++ filmsyl-frontend/app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import requests
 from datetime import datetime
-from data_source import call_api
+from data_source import get_api, post_api
+import pandas as pd
+from io import StringIO
 
 #base_url = f'https://taxifare.lewagon.ai/predict'
 base_url = 'https://films-you-like-2h7mcggcwa-ew.a.run.app'
 params = {}
 
-clusters = call_api(f'{base_url}/cluster', params)
+clusters = get_api(f'{base_url}/cluster', params)
+
 
 st.set_page_config(
             page_title="Quick reference", # => Quick reference - Streamlit
@@ -32,7 +35,47 @@ with netflix:
    st.header("A dog")
    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
+#@st.cache
 
+st.markdown("""
+
+    ## Please upload your Netflix History:
+
+""")
+
+uploaded_file = st.file_uploader('Upload CSV', accept_multiple_files=False, type=['csv'])
+
+
+if st.button('Upload File') and uploaded_file is not None:
+    try:
+        bytes_data = uploaded_file.getvalue()
+
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+        string_data = stringio.read()
+
+        dataframe = pd.read_csv(uploaded_file)
+        post_result = post_api(f'{base_url}/upload-netflix', params={}, data=dataframe.to_json())
+        st.text(post_result)
+        # Save the uploaded file
+        #with open(uploaded_file.name, "wb") as f:
+         #   f.write(uploaded_file.getbuffer())
+
+        # Display success message
+        st.success('File saved successfully')
+
+    except Exception as e:
+        st.error(f'An error occurred: {str(e)}')
+
+
+
+
+
+
+#if st.button("Upload PDF"):
+ #   payload = {"username": username, "filename": uploaded_file.name}
+  #  response = requests.post(base_url, params=payload, files={"uploaded_file": uploaded_file.getvalue()})
+   # st.write(response)
 
 
 with cinemas:
--- filmsyl-frontend/data_source.py
+++ filmsyl-frontend/data_source.py
@@ -1,8 +1,20 @@
 from datetime import datetime
 import requests
-def call_api(base_url: str, params):
+def get_api(base_url: str, params):
 
     result =  requests.get(base_url, params=params)
+    print(result.url)
+    if result.status_code < 300:
+        json = result.json()
+        return json
+    else:
+        print(f'Api call returned with code {result.status_code}')
+
+
+
+def post_api(base_url: str, params, data):
+
+    result =  requests.post(base_url, params=params, data=data)
     print(result.url)
     if result.status_code < 300:
         json = result.json()
