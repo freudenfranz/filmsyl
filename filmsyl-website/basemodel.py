7f8a7f948892863ab7713681403eb5fd68f53e67
Jakob Stimpfl <jakob.stimpfl@gmail.com>
2024-03-07 12:25:54 +0100

    cleaned up basemodel file

--- filmsyl-website/basemodel.py
+++ filmsyl-website/basemodel.py
@@ -10,12 +10,11 @@ from sklearn.neighbors import NearestNeighbors
 #import IMDb movie csv
 csv = os.path.join("/home/lambert/filmsyl/filmsyl/raw_data", "imdb_movies.csv")
 imdb_df = pd.read_csv(csv)
-imdb_df.replace({'\\N': np.nan, '': np.nan}, inplace=True)
 
 #import Netflix movie csv
 csv_ = os.path.join("/home/lambert/filmsyl/filmsyl/raw_data", "jakob_movies.csv")
 netflix_df = pd.read_csv(csv_)
-netflix_df.replace({'\\N': np.nan, '': np.nan}, inplace=True)
+
 
 #combine imdb text features.
 imdb_df['text_features']= imdb_df['genres'] + ' ' + imdb_df['title'] + ' ' + imdb_df['Director']
@@ -44,11 +43,10 @@ imdb_recommendations = []  # Initialize an empty list to store IMDb recommendati
 
 # Loop through each row in the DataFrame netflix_df
 for i in range(len(netflix_df)):
-    if len(indices[i]) > 1:  # Check if there are at least two neighbors
-        second_nearest_neighbor_index = indices[i][1]  # Get the index of the second closest neighbor for the current row
-        imdb_title = imdb_df.iloc[second_nearest_neighbor_index]['title']  # Retrieve the title from the IMDb DataFrame using the second nearest neighbor index
-        netflix_title = netflix_df.iloc[i]['title']  # Retrieve the title of the Netflix movie
-        imdb_recommendations.append((netflix_title, imdb_title))  # Append a tuple of Netflix title and IMDb recommendation
+    second_nearest_neighbor_index = indices[i][1]  # Get the index of the second closest neighbor for the current row
+    imdb_title = imdb_df.iloc[second_nearest_neighbor_index]['title']  # Retrieve the title from the IMDb DataFrame using the second nearest neighbor index
+    netflix_title = netflix_df.iloc[i]['title']  # Retrieve the title of the Netflix movie
+    imdb_recommendations.append((netflix_title, imdb_title))  # Append a tuple of Netflix title and IMDb recommendation
 
 # Display the first 10 IMDb recommendations
 for netflix_title, imdb_title in imdb_recommendations[:5]:
